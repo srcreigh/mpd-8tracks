873599fffd27185b5224565e5f7c7e497fe9c861
Owen Smith <owen@omsmith.ca>
2013-08-17 16:19:24 -0400

    use json instead

--- mpd-8tracks.py
+++ mpd-8tracks.py
@@ -25,8 +25,7 @@
 import sys
 import urllib2
 import os
-import xml.etree.ElementTree as ET
-import xml.dom
+import json
 
 # Open the API developer key
 api_key = open("8tracksdevkey.txt", 'r').readline()[:-1]
@@ -51,56 +50,44 @@ os.system("mpc clear 1>/dev/null")
 os.system("mpc consume on 1>/dev/null")
 
 # Get the mix information, extract the mix id
-query_url = "%s.xml?api_version=%s&api_key=%s" % (mix_url, api_version, api_key)
-mix_info = ET.fromstring(urllib2.urlopen(query_url).read())
+query_url = "%s.jsonp?api_version=%s&api_key=%s" % (mix_url, api_version, api_key)
+mix_info = json.loads(urllib2.urlopen(query_url).read())
 
-for info in mix_info[0]:
-   if info.tag == 'id':    mix_id = info.text
-   # Here we use .encode('ascii', 'ignore') to deal with unicode mix names
-   # (which come up kind of a lot in 8tracks playlists)
-   if info.tag == 'name':  mix_name = info.text.encode('ascii', 'ignore')
+mix_id = mix_info['mix']['id']
+mix_name = mix_info['mix']['name'].encode('ascii', 'ignore')
 
 os.system("mkdir \"playlists/%s\" 1>/dev/null 2>/dev/null" % mix_name)
 
 # Get the play token
-query_url = "http://8tracks.com/sets/new.xml?api_version=%s&api_key=%s" % (api_version, api_key)
-play_token_info = ET.fromstring(urllib2.urlopen(query_url).read())
-for n in play_token_info:
-   if n.tag == 'play-token': play_token = n.text
+query_url = "http://8tracks.com/sets/new.jsonp?api_version=%s&api_key=%s" % (api_version, api_key)
+play_token_info = json.loads(urllib2.urlopen(query_url).read())
+play_token = play_token_info['play_token']
 
 # Start the playlist
-query_url = "http://8tracks.com/sets/460486803/play.xml?api_version=%smix_id=" % api_version + mix_id
+query_url = "http://8tracks.com/sets/460486803/play.jsonp?api_version=%smix_id=%s" % (api_version, mix_id)
 query_url += "&api_key=" + api_key
 query_url += "&play_token=" + play_token
 
-song_info = ET.fromstring(urllib2.urlopen(query_url).read())
+song_info = json.loads(urllib2.urlopen(query_url).read())
 
 # Song playing loop
-last_song = False
-while not last_song:
+while True:
 
    # Get relevant information and save it
-   for i in song_info[0]:
-      if i.tag == 'at-end' and i.text == 'true':
-         last_song = True
-         continue
-      if i.tag == 'track':
-         for j in i:
-            if j.tag == 'id':          
-               track_id = j.text
-            elif j.tag == 'name':      
-               name = j.text.encode('ascii', 'ignore')
-            elif j.tag == 'performer': 
-               artist = j.text.encode('ascii', 'ignore')
-            elif j.tag == 'url':       
-               track_url = j.text
+   if song_info['set']['at_end']:
+      break
+
+   track_id = song_info['set']['track']['id']
+   name = song_info['set']['track']['name'].encode('ascii', 'ignore')
+   artist = song_info['set']['track']['name'].encode('ascii', 'ignore')
+   track_url = song_info['set']['track']['url']
 
    print "Playing: %s - \"%s\"" % (artist, name)
 
    # Notify 8tracks that the song is being played
    query_url = "http://8tracks.com/sets/%s/report.xml" % play_token
-   query_url += "?mix_id=" + mix_id
-   query_url += "&track_id=" + track_id
+   query_url += "?mix_id=%s" % mix_id
+   query_url += "&track_id=%s" % track_id
    query_url += "&api_key=" + api_key
    # note: do not need to save any information, just need to call the url
    urllib2.urlopen(query_url) 
@@ -118,7 +105,7 @@ while not last_song:
    os.system("mpc current --wait 1>/dev/null")
 
    # Load the next song
-   query_url = "http://8tracks.com/sets/%s/next.xml" % play_token
-   query_url += "?mix_id=" + mix_id
+   query_url = "http://8tracks.com/sets/%s/next.jsonp" % play_token
+   query_url += "?mix_id=%s" % mix_id
    query_url += "&api_key=" + api_key
-   song_info = ET.fromstring(urllib2.urlopen(query_url).read())
+   song_info = json.loads(urllib2.urlopen(query_url).read())
